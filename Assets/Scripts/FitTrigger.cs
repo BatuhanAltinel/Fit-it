a59471f7cf90eff2736b52059bab4b64121a2043
DESKTOP-UFGH3NC\ELİF <batuhan.altinel@hotmail.com>
2022-07-20 13:12:16 +0300

    Object in right place or wrong place added.

--- Assets/Scripts/FitTrigger.cs
+++ Assets/Scripts/FitTrigger.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class FitTrigger : MonoBehaviour
 {
     public string moldTag;
+    private bool isTriggered;
+    public ParticleSystem fitParticle;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        isTriggered = false;
+    }
+    void Update()
+    {
+        DontFitIt();
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag(moldTag))
         {
-            Debug.Log("Cube triggered "+ moldTag +" mold.");
+            Debug.Log("triggered "+ moldTag +" mold.");
+            isTriggered = true;
+            //fitParticle.Play();
             //SoundEffect;
             //ParticleEffect;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("FitTrigger") && isTriggered)
+        {
+            Debug.Log("FÝT TRÝGGERED");
+            fitParticle.Play();
+        }
+    }
+
+    void DontFitIt()
+    {
+        if (this.transform.position.y < -9.1f && !isTriggered )
+        {
+            Debug.Log("dont fit the right place");
+            StartCoroutine("WrongplaceDestroyObject");
+        }
+    }
+
+    IEnumerator WrongplaceDestroyObject()
+    {
+        this.transform.parent.parent.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+    }
+
 }
