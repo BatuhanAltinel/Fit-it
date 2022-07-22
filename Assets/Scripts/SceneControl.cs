6219c7a34101784ddd782407d3bc476066e7cb42
DESKTOP-UFGH3NC\ELİF <batuhan.altinel@hotmail.com>
2022-07-22 22:39:10 +0300

    Menu scene added

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneControl : MonoBehaviour
+{
+    public static SceneControl instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
